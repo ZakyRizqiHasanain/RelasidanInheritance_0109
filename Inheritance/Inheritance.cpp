c4204c42e07291f8724febdd78b0a7feaa5d9b91
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-05-27 19:59:56 +0700

    mmebuat class pelajar

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -19,3 +19,18 @@ class orang {
         }
 };
 
+class pelajar : public orang {
+public:
+    string sekolah;
+    
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallon nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+    }
+};
+
